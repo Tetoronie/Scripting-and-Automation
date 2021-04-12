3bbdcf6b6e1bfd11bf13ad8a1ab4033451844d5c
tetoronie <chrismathieson25@gmail.com>
2021-04-12 13:29:00 -0400

    Working import function, script is finished

--- PowerCli.ps1
+++ PowerCli.ps1
@@ -3,23 +3,10 @@ $user = read-host -Prompt "Please enter username:"
 $pass = read-host -Prompt "Please enter password:"
 
 function connect {
+    # Connect to the vcsa server with user provided credentials
     Connect-VIServer -Server $vServer -User $user -Password $pass
 }
 
-
-function listProfile {
-
-    Get-vmhostprofile
-}
-
-function startAll {
-
-    $list = Get-VM
-
-    foreach($vm in $list) {
-        Start-VM -VM $vm -Confirm:$false
-    }
-}
 
 function exportOVA {
 
@@ -44,64 +31,51 @@ function importOVA {
 
     #Select host
     get-vmhost
-    $host = read-host -Prompt "Select VMHost:"
+    $vhost = read-host -Prompt "Select VMHost:"
 
     #Select datastore
-    get-datastore | select-object -Property Name, ID
+    get-vmhost -Name $vhost | get-datastore | select-object -Property Name
     $datastore = read-host -Prompt "Select Datastore:"
     
 
-
+    # Set name of VM and how many to create
     $name = read-host -Prompt "Set name:"
     $loop = read-host -Prompt "How many to deploy:"
 
     if ($loop -gt 1) {
+        # Loop for deploying more than one of OVA
+	for ($n=1; $n -le $loop; $n++) {
 
-        $n = 1
+		$nameN = ($name + $n)
+		import-vapp -source $Source -VMHost $vhost -Datastore $datastore -Name $nameN
 
-       }
-    while ($loop -gt 0) {
-
-        $nameN = ('$name' + '$n')
-
-        import-vapp -source $Source -VMHost $host -Datastore $datastore -Name $nameN
-
-        $n + 1
-        $loop - 1
+	}   
     }
+    else {
+	# Deploy single OVA
+	import-vapp -source $Source -VMHost $vhost -Datastore $datastore -Name $name	
+
+	}
 }
 
 
 function menu {
+
+    # Display menu for user selection
     Write-Host "==Select an option=="
     write-host "1) Export OVAs"
     Write-Host "2) Import OVAs"
     $select = Read-Host -Prompt "Selection:"
 
-
-
-<#
-if (1 -eq $select ) {
-
-    exportOVA
-}
-
-elseif (2 -eq $select) {
-
-    importOVA
-}
-#>
-
+# Switch statement calls OVA function based on user input
 switch ( $select )
 {
-	1 { write-host "1"
-		exportOVA }
+	1 { exportOVA }
 	2 { importOVA }
 
 }
 
 }
 
-#connect
-
-menu 
+connect
+menu
